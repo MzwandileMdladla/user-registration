2ff6531c5985461ed76edcb9e9c02c81183f768b
MzwandileMdladla <Mzwandile.Mdladla@absa.africa>
2021-10-24 16:11:53 +0200

    Added token confirmation

--- src/main/java/za/co/jengo/demo/registration/RegistrationController.java
+++ src/main/java/za/co/jengo/demo/registration/RegistrationController.java
@@ -1,9 +1,11 @@
 package za.co.jengo.demo.registration;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +19,10 @@ public class RegistrationController {
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
+
+    @GetMapping(path = "confirm")
+    public String confirm(@RequestParam("token") String token) {
+        return registrationService.confirmToken(token);
+    }
+
 }
--- src/main/java/za/co/jengo/demo/registration/RegistrationService.java
+++ src/main/java/za/co/jengo/demo/registration/RegistrationService.java
@@ -2,9 +2,14 @@ package za.co.jengo.demo.registration;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import za.co.jengo.demo.appuser.AppUser;
 import za.co.jengo.demo.appuser.AppUserRoleEnum;
 import za.co.jengo.demo.appuser.AppUserService;
+import za.co.jengo.demo.registration.token.ConfirmationToken;
+import za.co.jengo.demo.registration.token.ConfirmationTokenService;
+
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
@@ -12,6 +17,7 @@ public class RegistrationService {
 
     private final AppUserService appUserService;
     private final EmailValidator emailValidator;
+    private final ConfirmationTokenService confirmationTokenService;
 
     public String register(final RegistrationRequest request) {
         boolean isValidEmail = emailValidator.test(request.getEmail());
@@ -25,5 +31,28 @@ public class RegistrationService {
                 request.getPassword(),
                 AppUserRoleEnum.USER
         ));
+    }
+
+    @Transactional
+    public String confirmToken(final String token) {
+        ConfirmationToken confirmationToken = confirmationTokenService
+                .getToken(token)
+                .orElseThrow(() -> new IllegalStateException("Token not found"));
+
+        if (confirmationToken.getConfirmedAt() != null) {
+            throw new IllegalStateException("Email already confirmed");
+        }
+
+        LocalDateTime expiredAt = confirmationToken.getExpiresAt();
+
+        if (expiredAt.isBefore(LocalDateTime.now())) {
+            throw new IllegalStateException("Token expired");
+        }
+
+        confirmationTokenService.setConfirmedAt(token);
+
+        appUserService.enableAppUser(confirmationToken.getAppUser().getEmail());
+
+        return "Confirmed";
     }
 }
